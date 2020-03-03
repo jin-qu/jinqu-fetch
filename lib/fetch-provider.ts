23609e7f8835d946267a7a7e9ab89660823a6912
Umut Özel <umutozel@gmail.com>
2020-03-03 10:03:14 +0300

    fix linter errors

--- lib/fetch-provider.ts
+++ lib/fetch-provider.ts
@@ -1,34 +1,36 @@
-import { IAjaxProvider, AjaxOptions, mergeAjaxOptions, Value, AjaxResponse } from "jinqu";
+import { AjaxOptions, AjaxResponse, IAjaxProvider, mergeAjaxOptions, Value } from "jinqu";
 
 export class FetchProvider implements IAjaxProvider<Response> {
 
-    static readonly defaultOptions: AjaxOptions = {
-        method: 'GET',
+    public static readonly defaultOptions: AjaxOptions = {
         headers: {
-            'Accept': 'application/json; charset=utf-8',
-            'Content-Type': 'application/json; charset=utf-8'
-        }
+            "Accept": "application/json; charset=utf-8",
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        method: "GET",
     };
 
-    ajax<T>(o: AjaxOptions): Promise<Value<T> & AjaxResponse<Response>> {
+    public ajax<T>(o: AjaxOptions): Promise<Value<T> & AjaxResponse<Response>> {
         if (o.params && o.params.length) {
-            o.url += '?' + o.params.map(p => `${p.key}=${encodeURIComponent(p.value)}`).join('&');
+            o.url += "?" + o.params.map((p) => `${p.key}=${encodeURIComponent(p.value)}`).join("&");
         }
-        
-        const p = fetch(o.url, createRequest(o))
-            .then(r => {
+
+        const promise = fetch(o.url, createRequest(o))
+            .then((r) => {
                 return r.json()
-                    .then(d => ({ value: d, response: r }));
+                    .then((d) => ({ value: d, response: r }));
             });
 
-        if (!o.timeout) return <any>p;
+        if (!o.timeout) {
+            return promise as any;
+        }
 
-        return <any>Promise.race([
-            p,
+        return Promise.race([
+            promise,
             new Promise((_, reject) =>
-                setTimeout(() => reject(new Error('Request timed out')), o.timeout)
-            )
-        ]);
+                setTimeout(() => reject(new Error("Request timed out")), o.timeout),
+            ),
+        ]) as any;
     }
 }
 
@@ -36,9 +38,9 @@ export function createRequest(o: AjaxOptions) {
     const d = Object.assign({}, FetchProvider.defaultOptions);
     o = mergeAjaxOptions(d, o);
 
-    return <RequestInit>{
+    return {
         body: JSON.stringify(o.data),
         headers: o.headers,
-        method: o.method
-    };
+        method: o.method,
+    } as RequestInit;
 }
