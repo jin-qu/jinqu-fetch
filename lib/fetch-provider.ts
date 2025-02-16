fc980ea7efb1b9a1fa8e64e5304039b76b491730
Umut Özel <umutozel@gmail.com>
2025-02-17 00:22:02 +0300

    publish and fix latest npm

--- lib/fetch-provider.ts
+++ lib/fetch-provider.ts
@@ -40,7 +40,7 @@ export function createRequest(o: FetchOptions) {
     const ao = Object.fromEntries(Object.entries(oo).filter(([key]) => key[0] != "$"));
 
     ao.method = ao.method || oo.$method;
-    if (oo.$data != null && oo.$method != "GET") {
+    if (oo.$data != null && ao.$method != "GET") {
         ao.body = JSON.stringify(oo.$data);
     }
     if (oo.$headers != null) {
